48ca076053b9e48b809713cc46c0cf4b84f95887
JavaConterry <conterry05@gmail.com>
2022-01-24 19:59:51 +0200

    Fast Fibonacci

--- src/Fib.java
+++ src/Fib.java
@@ -1,6 +1,7 @@
 import java.util.HashMap;
 import java.util.Map;
 
+
 public class Fib {
 
     Map< Integer, Integer> cache = new HashMap<Integer, Integer>();
@@ -11,11 +12,27 @@ public class Fib {
 
     }
 
-    public int fibOf(int numOfFibNumber){
 
+    public int fibOf(int numOfFibNumber){
+        int result;
         try {
             if(cache.get(numOfFibNumber) == null){
-                cache.put(numOfFibNumber, (fibOf(numOfFibNumber - 2) + fibOf(numOfFibNumber - 1)) % 1_000_000_000);
+
+                if(numOfFibNumber % 5 == 0)
+                {
+                    result = (int)fibOf5n(numOfFibNumber)% 1_000_000_000;
+                }
+                else if(numOfFibNumber % 3 == 0){
+                    result = (int)fibOf3n(numOfFibNumber)% 1_000_000_000;
+                }
+                else if(numOfFibNumber % 2 == 0){
+                    result = fibOf2n(numOfFibNumber)% 1_000_000_000;
+                }
+                else{
+                    result = (fibOf(numOfFibNumber - 2) + fibOf(numOfFibNumber - 1))% 1_000_000_000;
+                }
+
+                cache.put(numOfFibNumber, result);
             }
             return cache.get(numOfFibNumber);
         }
@@ -23,6 +40,28 @@ public class Fib {
             return  fibOf(numOfFibNumber-1);
         }
     }
+
+    private double fibOf5n(int n){
+        int a = fibOf(n/5);
+        return 25*a*a*a*a*a + 25*Math.pow(-1, n)*a*a*a + 5*a;
+    }
+
+    private double fibOf3n(int n){
+        int a = fibOf(n/3+1);
+        int b = fibOf(n/3);
+        int c = fibOf(n/3-1);
+
+        return a*a*a + b*b*b - c*c*c;
+    }
+
+    private int fibOf2n(int n){
+        int a = fibOf(n/2+1);
+        int b = fibOf(n/2-1);
+
+        return a*a - b*b;
+    }
+
+
 
 }
 
